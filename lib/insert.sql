75be744860299d36a58665dd772a82956e5b437f
Christine Cua <cloudycua@gmail.com>
2018-04-27 13:37:36 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "M", "Brown", "Mellow", "True")
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "M", "Black", "Rowdy", "True")
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 1, "F", "Honey", "Friendly", "True")
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 1, "M", "Brown", "Antsy", "True")
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 1, "F", "Black", "Mellow", "True")
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 1, "M", "Green", "Cagey", "False")
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 1, "F", "Green", "Friendly", "True")
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, NULL)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "M", "Brown", "Mellow", "True");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "M", "Black", "Rowdy", "True");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 1, "F", "Honey", "Friendly", "True");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 1, "M", "Brown", "Antsy", "True");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 1, "F", "Black", "Mellow", "True");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 1, "M", "Green", "Cagey", "False");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 1, "F", "Green", "Friendly", "True");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, NULL);
